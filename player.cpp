394f43dec7e515bed58092358953a67408e62ea0
MarlonFacey <mfacey6@yahoo.com>
2017-03-06 22:43:37 -0800

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -37,7 +37,12 @@ int Player::minimax(Board *board, Side side, int depth, Move *best_move)
 	{
 		return board->heuristic_score(side);
 	}
-	int best_score = -1000000; 
+	if (depth%2 == 0)
+	{int best_score = -1000000; 
+	}
+	else {
+	int best_score = 1000000; 
+	}
 	int new_score; 
 	Move *m0, *m1; 
 	Board *new_board; 
